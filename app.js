45d2c8d5ef361071c7dcb71661dc5e8c2b384908
Shiba-Patrick <liu21407@gmail.com>
2023-02-16 16:32:02 +0800

    add POST function on edit/new and delete

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const express_hbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 
 const app = express()
 const port = 3000
@@ -8,19 +9,40 @@ const port = 3000
 app.engine('handlebars', express_hbs({defaultLayout:'main'}))
 app.set('view engine', 'handlebars')
 
+app.use(bodyParser.urlencoded({ extended: true })) //連結post傳入
+
 //home page
 app.get('/', (req, res)=>{
   res.render('index')
 })
 
 //new page
-app.get('/new', (req, res) => {
+app.get('/new', (req, res)=>{
   res.render('new')
 })
 
+//post new table
+app.post('/new',(req, res)=>{
+  const body = req.body
+  console.log(body) //test
+  res.render('new', {body})
+})
+
 //edit page
-app.get('/edit', (req, res) => {
+app.get('/edit', (req, res)=>{
   res.render('edit')
+})
+
+//post edit table
+app.post('/edit', (req, res) => {
+  const body = req.body
+  console.log(body) //test
+  res.render('edit', { body })
+})
+
+//delete function
+app.get('/delete', (req, res) => {
+  res.redirect('/')
 })
 
 app.listen(port, () => {
