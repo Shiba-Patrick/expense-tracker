cf64dc89d7d699d77e0683a526c94e469f62dcfe
Shiba-Patrick <liu21407@gmail.com>
2023-02-16 16:02:50 +0800

    add main/index/new/edit page connect index

--- app.js
+++ app.js
@@ -8,8 +8,19 @@ const port = 3000
 app.engine('handlebars', express_hbs({defaultLayout:'main'}))
 app.set('view engine', 'handlebars')
 
+//home page
 app.get('/', (req, res)=>{
   res.render('index')
+})
+
+//new page
+app.get('/new', (req, res) => {
+  res.render('new')
+})
+
+//edit page
+app.get('/edit', (req, res) => {
+  res.render('edit')
 })
 
 app.listen(port, () => {
