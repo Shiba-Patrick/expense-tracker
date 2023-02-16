ee8e8b5e8b616771fd3ec0cf378c8a497568acf4
Shiba-Patrick <liu21407@gmail.com>
2023-02-16 17:09:42 +0800

    add new function and build mongooseDB

--- app.js
+++ app.js
@@ -1,35 +1,72 @@
 const express = require('express')
 const express_hbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const Record = require('./models/record')
+
+// 非正式環境下使用dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+// mongoose setting
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongoDB error!!!!!!!')
+})
+db.once('open', () => {
+  console.log('mongoDB connected!!!')
+})
 
 const app = express()
 const port = 3000
 
 //handlebars模板引勤
-app.engine('handlebars', express_hbs({defaultLayout:'main'}))
+app.engine('handlebars', express_hbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(bodyParser.urlencoded({ extended: true })) //連結post傳入
 
 //home page
-app.get('/', (req, res)=>{
-  res.render('index')
+app.get('/', (req, res) => {
+  Record.find()
+    .lean()
+    .sort({ _id: 'asc' })
+    .then(items => {
+      items.forEach(item => {
+        item.date = item.date.toISOString().slice(0, 10)
+      })
+      console.log(items)//test
+      return items
+    })
+    .then(records => {
+      res.render('index', { records })
+    })
+    .catch(error => console.log(error))
 })
 
 //new page
-app.get('/new', (req, res)=>{
+app.get('/new', (req, res) => {
   res.render('new')
 })
 
 //post new table
-app.post('/new',(req, res)=>{
+app.post('/new', (req, res) => {
   const body = req.body
-  console.log(body) //test
-  res.render('new', {body})
+  console.log(body)//test
+  Record.create({
+    name: body.name,
+    date: body.date,
+    category: body.category,
+    cost: body.cost
+  })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 //edit page
-app.get('/edit', (req, res)=>{
+app.get('/edit', (req, res) => {
   res.render('edit')
 })
 
