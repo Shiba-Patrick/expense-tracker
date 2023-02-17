034642a676c996cbac46bb522e790caaa7a5e884
Shiba-Patrick <liu21407@gmail.com>
2023-02-17 13:20:22 +0800

    add search function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Record = require('../../models/record')
 const Category = require('../../models/category')
+const record = require('../../models/record')
 
 //home page
 router.get('/', async (req, res) => {
@@ -15,6 +16,27 @@ router.get('/', async (req, res) => {
     return item
   }))
   records.totalAmount = totalAmount
+  res.render('index', { records })
+})
+
+//search-bar function
+router.post('/search',async (req, res)=>{
+  let totalAmount = 0
+  const search = req.body.search
+  const categoryItem =await Category.findOne({ name: search})
+  const categoryId = categoryItem._id
+  const allRecords = await Record.find().lean().sort({ _id: 'asc'})
+  const filterRecords = allRecords.filter(record =>{
+    return (JSON.stringify(record.categoryId)) === (JSON.stringify(categoryId))
+  })
+  const records = filterRecords.map(record =>{
+    record.categoryIcon = categoryItem.icon
+    record.date = record.date.toISOString().slice(0 ,10)
+    totalAmount += record.cost
+    return record
+  })
+  records.totalAmount =totalAmount
+  records.categoryName = categoryItem.name
   res.render('index', { records })
 })
 
